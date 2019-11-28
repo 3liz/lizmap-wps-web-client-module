25eb44e7041bbd498388d2c0629dabc205cc0be1
David Marteau <dmarteau@3liz.com>
2019-11-28 18:32:13 +0100

    Remove unwanted files

--- tests/lizmap/var/lizmap-config/liveconfig.ini.php
+++
@@ -1,3 +0,0 @@
-;<?php die('');?> live configuration
-[coordplugin_auth]
-persistant_crypt_key=cWyxYS6untJRIB1I173rcLm4u8TAWv
--- tests/lizmap/www/Proj4js/defs/ESRI54016.js
+++
@@ -1,1 +0,0 @@
-Proj4js.defs["ESRI:54016"] = "+proj=gall +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
