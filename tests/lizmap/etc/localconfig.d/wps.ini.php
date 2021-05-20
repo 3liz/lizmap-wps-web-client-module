0aa7680473c6a8df290fe792a212c287da0acd6c
David Marteau <dmarteau@3liz.com>
2021-05-20 10:37:34 +0200

    Add supports for drop-in configuration
        - Available in lizmap 3.4/3.5

---
+++ tests/lizmap/etc/localconfig.d/wps.ini.php
@@ -0,0 +1,12 @@
+[modules]
+wps.access=2
+  
+[wps]
+wps_rootUrl="http://wps:8080/"
+ows_url="/http://map:8080/ows/"
+wps_rootDirectories="/srv/projects"
+redis_host=redis
+redis_port=6379
+redis_db=1
+redis_key_prefix=wpslizmap
+
