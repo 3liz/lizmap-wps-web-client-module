a53cacaf9b7ac1b5bf1eae9e7ea3df13ab973213
rldhont <rldhont@gmail.com>
2023-01-17 17:07:06 +0100

    Tests: enhancing Test input file destination algorithm

--- tests/lizmap/processing/algorithms/TestFileDestination.py
+++ tests/lizmap/processing/algorithms/TestFileDestination.py
@@ -1,5 +1,6 @@
 """ Test file destination parsing
 """
+import codecs
 
 from qgis.core import (QgsProcessingParameterNumber,
                        QgsProcessingParameterString,
@@ -23,7 +24,7 @@ class TestFileDestination(QgsProcessingAlgorithm):
         return 'Test input file destination'
 
     def createInstance(self, config={}):
-        """ Virtual override 
+        """ Virtual override
 
             see https://qgis.org/api/classQgsProcessingAlgorithm.html
         """
@@ -35,12 +36,21 @@ class TestFileDestination(QgsProcessingAlgorithm):
             see https://qgis.org/api/classQgsProcessingAlgorithm.html
         """
         self.addParameter(QgsProcessingParameterFileDestination(self.OUTPUT,
-            'JSON file',
-            'JSON Files (*.json)'
+            'Text file',
+            'TEXT Files (*.txt)'
         ))
 
     def processAlgorithm(self, parameters, context, feedback):
-        
-        value = parameters[self.OUTPUT]
-        return {self.OUTPUT: value}
-        
+
+        outputTxtFile = self.parameterAsFileOutput(parameters, self.OUTPUT, context)
+
+        # Prepare results
+        results = {
+            self.OUTPUT: None
+        }
+
+        with codecs.open(outputTxtFile, 'w', encoding='utf-8') as f:
+            f.write('Test input file destination result!')
+            results[self.OUTPUT] = outputTxtFile
+
+        return results
