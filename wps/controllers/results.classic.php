fdb1847a346d8819f3d8410c1f2aa59b52512418
rldhont <rldhont@gmail.com>
2023-07-04 13:42:38 +0200

    Fix: results controller

--- wps/controllers/results.classic.php
+++ wps/controllers/results.classic.php
@@ -22,16 +22,19 @@ class resultsCtrl extends jController
         // wps process identifier
         $identifier = $this->param('identifier');
         if (!$identifier) {
+            jLog::log('results controller, index: No identifier param', 'errors');
             return $rep;
         }
 
         // lizmap project and repository
         $repository = $this->param('repository');
         if (!$repository) {
+            jLog::log('results controller, index: No repository param', 'errors');
             return $rep;
         }
         $project = $this->param('project');
         if (!$project) {
+            jLog::log('results controller, index: No project param', 'errors');
             return $rep;
         }
 
@@ -60,23 +63,30 @@ class resultsCtrl extends jController
         // wps process identifier
         $identifier = $this->param('identifier');
         if (!$identifier) {
+            jLog::log('results controller, update: No identifier param', 'errors');
             return $rep;
         }
 
         // lizmap project and repository
         $repository = $this->param('repository');
         if (!$repository) {
+            jLog::log('results controller, update: No repository param', 'errors');
             return $rep;
         }
         $project = $this->param('project');
         if (!$project) {
+            jLog::log('results controller, update: No project param', 'errors');
             return $rep;
         }
 
         // Get and decode json post
         $requestJson = $this->param('__httpbody');
+        if (!$requestJson) {
+            $requestJson = json_decode($this->request->getBody(), true);
+        }
 
         if (!$requestJson) {
+            jLog::log('results controller, update: No request JSON', 'errors');
             return $rep;
         }
         // depending on how jelix is configured, we could have directly
@@ -84,6 +94,7 @@ class resultsCtrl extends jController
         $data = (is_string($requestJson) ? json_decode($requestJson, true) : $requestJson);
 
         if (!array_key_exists('uuid', $data)) {
+            jLog::log('results controller, update: No uuid in request JSON');
             return $rep;
         }
 
@@ -106,16 +117,19 @@ class resultsCtrl extends jController
         // wps process identifier
         $identifier = $this->param('identifier');
         if (!$identifier) {
+            jLog::log('results controller, delete: No identifier param', 'errors');
             return $rep;
         }
 
         // lizmap project and repository
         $repository = $this->param('repository');
         if (!$repository) {
+            jLog::log('results controller, delete: No repository param', 'errors');
             return $rep;
         }
         $project = $this->param('project');
         if (!$project) {
+            jLog::log('results controller, delete: No project param', 'errors');
             return $rep;
         }
 
