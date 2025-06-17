e584a07e3550493cb4e75fabb3ab8b2c42e257a3
neo <neo.konikoff-garaix01@etu.umontpellier.fr>
2025-06-17 12:10:22 +0200

    ADD: Tests E2E for API

---
+++ tests/end2end/tests/auth.setup.js
@@ -0,0 +1,16 @@
+import { test as setup } from '@playwright/test';
+import { authUsingLogin, getAuthStorageStatePath } from './globals';
+import {LizmapEditionWPSURLPage} from "./pom/lizmap-edit-wps-url-page";
+
+async function initializeServersURLs(page, wps, map) {
+    const lizmapEditionWPS_URL_Page = new LizmapEditionWPSURLPage(page);
+    await lizmapEditionWPS_URL_Page.initializeURLS(wps, map);
+}
+
+setup('authenticate as admin and add map', async ({ page }) => {
+    const WPS_URL = "http://tests-wps-1:8080/";
+    const MAP_URL = "";
+
+    await authUsingLogin(page, 'admin', 'admin', getAuthStorageStatePath('admin'));
+    await initializeServersURLs(page, WPS_URL, MAP_URL);
+});
